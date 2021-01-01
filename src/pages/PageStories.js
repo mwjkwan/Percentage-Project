5e183e96c88db63be6ffe1bd06c39fb691b17814
Melissa Kwan <mwjkwan@gmail.com>
2021-01-01 14:07:52 -0800

    Add theming; replace PageStories with template

--- src/pages/PageStories.js
+++ src/pages/PageStories.js
@@ -1,9 +1,38 @@
-function PageStories() {
+import React, { useState } from 'react';
+import { Scrollama, Step } from 'react-scrollama';
+const PageStories = () => {
+  // TODO: read https://reactjs.org/docs/hooks-overview.html for context
+  const [currentStepIndex, setCurrentStepIndex] = useState(null);
+  // This callback fires when a Step hits the offset threshold. It receives the
+  // data prop of the step, which in this demo stores the index of the step.
+  const onStepEnter = ({ data }) => {
+    setCurrentStepIndex(data);
+  };
   return (
-    <div>
-        PageStories
+    <div style={{ margin: '50vh 0', border: '2px dashed skyblue' }}>
+      <div style={{ position: 'sticky', top: 0, border: '1px solid orchid' }}>
+        {/* TODO: Replace this with fixed things */}
+        I'm sticky. The current triggered step index is: {currentStepIndex}
+      </div>
+      <Scrollama onStepEnter={onStepEnter} debug>
+        {/* TODO: What should [1, 2, 3, 4] be replaced with? */}
+        {/* TODO: Read for context on map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */}
+        {[1, 2, 3, 4].map((_, stepIndex) => (
+          <Step data={stepIndex} key={stepIndex}>
+            <div
+              style={{
+                margin: '50vh 0',
+                border: '1px solid gray',
+                opacity: currentStepIndex === stepIndex ? 1 : 0.2,
+              }}
+            >
+              {/* TODO: This div contains the scrolling elements. What should you replace this with? */}
+              I'm a Scrollama Step of index {stepIndex}
+            </div>
+          </Step>
+        ))}
+      </Scrollama>
     </div>
   );
-}
-
+};
 export default PageStories;
