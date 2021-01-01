1fbefd7e0caa50487b0cf18849fc2056e9531635
Melissa Kwan <mwjkwan@gmail.com>
2021-01-01 14:29:20 -0800

    Install react-scollama; add columns to PageStories

--- src/pages/PageStories.js
+++ src/pages/PageStories.js
@@ -1,5 +1,26 @@
 import React, { useState } from 'react';
 import { Scrollama, Step } from 'react-scrollama';
+
+const styles = {
+  outer: {
+    display: 'flex',
+  },
+  sticky: {
+    flex: 1,
+    position: 'sticky',
+    top: 0,
+    border: '3px solid orchid',
+    maxHeight: '100vh',
+  },
+  scrollText: {
+    width: '50vw',
+  },
+  step: {
+    margin: '50vh 0',
+    border: '1px solid gray',
+  }
+}
+
 const PageStories = () => {
   // TODO: read https://reactjs.org/docs/hooks-overview.html for context
   const [currentStepIndex, setCurrentStepIndex] = useState(null);
@@ -8,30 +29,28 @@ const PageStories = () => {
   const onStepEnter = ({ data }) => {
     setCurrentStepIndex(data);
   };
+
   return (
-    <div style={{ margin: '50vh 0', border: '2px dashed skyblue' }}>
-      <div style={{ position: 'sticky', top: 0, border: '1px solid orchid' }}>
-        {/* TODO: Replace this with fixed things */}
+    <div style={styles.outer}>
+      <div style={styles.sticky}>
+        {/* TODO: Replace this with fixed images that change based on currentStepIndex */}
         I'm sticky. The current triggered step index is: {currentStepIndex}
       </div>
-      <Scrollama onStepEnter={onStepEnter} debug>
-        {/* TODO: What should [1, 2, 3, 4] be replaced with? */}
-        {/* TODO: Read for context on map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */}
-        {[1, 2, 3, 4].map((_, stepIndex) => (
-          <Step data={stepIndex} key={stepIndex}>
-            <div
-              style={{
-                margin: '50vh 0',
-                border: '1px solid gray',
-                opacity: currentStepIndex === stepIndex ? 1 : 0.2,
-              }}
-            >
-              {/* TODO: This div contains the scrolling elements. What should you replace this with? */}
-              I'm a Scrollama Step of index {stepIndex}
-            </div>
-          </Step>
-        ))}
-      </Scrollama>
+      <div style={styles.scrollText}>
+        {/* In order to get rid of the dotted lines, delete "debug" */}
+        <Scrollama onStepEnter={onStepEnter} debug>
+          {/* TODO: What should [1, 2, 3, 4] be replaced with? */}
+          {/* TODO: Read for context on map: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */}
+          {[1, 2, 3, 4].map((_, stepIndex) => (
+            <Step data={stepIndex} key={stepIndex}>
+              <div style={{ ...styles.step, opacity: currentStepIndex === stepIndex ? 1 : 0.2 }}>
+                {/* TODO: This div contains the scrolling elements. What should you replace this with? */}
+                I'm a Scrollama Step of index {stepIndex}
+              </div>
+            </Step>
+          ))}
+        </Scrollama>
+      </div>
     </div>
   );
 };
