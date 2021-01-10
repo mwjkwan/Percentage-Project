6bdf3d98a46be734affb9c5ac16305a574309fc9
nadinem100 <67973324+nadinem100@users.noreply.github.com>
2021-01-10 14:02:14 -0500

    more styling

--- src/constants/people.js
+++ src/constants/people.js
@@ -14,7 +14,7 @@ const robin = {
     concentration: 'Statistics',
     quotes: ["I remember I was at office hours for Stat 110. And I was trying to ask the student sitting in front of me, like, \"What'd you get for number 3? I got 5.\" And he said, \"What?\" and was talking to me very disrespectfully and threw in the word \"dumb\" or something. I was just like, \"Are you not going to tell me how to do it?\" I don't know, it was just very disheartening. I kind of just left office hours, but it gave me a really bad impression of the department and the students in the department."],
     pic: robin_pic,
-    color: 'light blue', //should probs be like an rbg value or something but idk will figure that out
+    color: '#3D89BF',
     pronouns: 'she/her'
     //maybe also their assigned data viz graph?
 }
@@ -22,30 +22,30 @@ const robin = {
 const grace = {
     name: 'Grace Tian',
     year: '2022',
-    concentration: 'CS + Statistics',
+    concentration: 'Computer Science + Statistics',
     quotes: ["I think that's what makes people feel the most alone, doing CS or any field in general, when they have questions and they're treated with impatience or annoyance. So in general just being treated with more patience and friendliness when it comes to learning these new things for the first time."],
     pic: grace_pic,
-    color: 'orange',
+    color: '#DD6552',
     pronouns: 'she/her'
 }
 
 const sarika = {
     name: 'Sarika Chawla',
     year: '2023',
-    concentration: 'CS',
+    concentration: 'Computer Science',
     quotes: ["This isn't necessarily only specific to CS but to academics in general at Harvard, that if you need help on a problem set or if you are stuck on understanding some concept you're expected to constantly go to office hours or find study groups. And that's not always possible for disabled people because personally, I have a medical schedule. I have other schedules related to my disability that I have to follow...That kind of bars me from going to office hours anytime I want to go. Not to mention that office hours aren't always in accessible locations or they're in places where the elevator randomly breaks and no one knows about it or they're just far away on campus and by the time I get there office hours will be over. "],
     pic: sarika_pic,
-    color: 'dark blue',
+    color: '#EE8692',//'#0248A8',
     pronouns: 'she/her'
 }
 
 const cindy = {
     name: 'Cindy Wang',
     year: '2024',
-    concentration: 'CS/Pre-med',
+    concentration: 'Computer Science/Pre-med',
     quotes: ["At Harvard, if I need help, I can find help", "I feel like I have to like work a little harder to prove myself.", "With AI, I’d try to find new interpretations and like new perspectives from the the data, i think that would be really, really cool"],
     pic: cindy_pic,
-    color: 'navy green',
+    color: '#8f4c39',
     pronouns: 'she/her'
 }
 
@@ -55,7 +55,7 @@ const eric = {
     concentration: 'Environmental Engineering',
     quotes: ["I've always wanted to put science and my work in STEM into a broader context. And so for me, when we talk about climate change in ESE, there's the very scientific perspective of: this is how greenhouse gases are warming the planet. But it's then helpful as a Black man to be able to put that conversation in the context of discussing communities that will continue to be overburdened if we continue this path. If we talk about chemical contaminants and drinking water, I can bring the perspective of talking very clearly using my background in terms of how that's going to impact certain communities disproportionately."],
     pic: eric_pic,
-    color: 'beige',
+    color: '#9b938f',
     pronouns: 'he/him'
 }
 
@@ -65,7 +65,7 @@ const miela = {
     concentration: 'Statistics',
     quotes: ["Harvard didn't really give me the opportunity to see STEM applied to communities of color. There's a lot of really cool startups, especially startups founded by women on campus, that are geared towards minorities, geared towards women-centric issues."],
     pic: miela_pic,
-    color: 'black/grey',
+    color: '#3a627b',
     pronouns: 'she/her'
 }
 
