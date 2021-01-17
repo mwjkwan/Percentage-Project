21d842b8e45ec410db276fb169eb87ae8394042f
nadinem100 <67973324+nadinem100@users.noreply.github.com>
2021-01-17 15:06:55 -0500

    changed offest to 0.5 and map to iterate from people object

--- src/pages/PageStories.js
+++ src/pages/PageStories.js
@@ -36,28 +36,9 @@ const PageStories = () => {
   // const [offset, setOffset] = useState(0); //should this be 0.3?
   // This callback fires when a Step hits the offset threshold. It receives the
   // data prop of the step, which in this demo stores the index of the step.
-  let scrolling_down = true;
-  console.log("WHAT ABOUT NOOWWW")
   const onStepEnter = ({ data, direction }) => {
     setCurrentStepIndex(data);
-    console.log('HELOOOOOO')
-    if (direction == 'up'){
-      scrolling_down = true
-      console.log(scrolling_down)
-    } else{
-      scrolling_down = false
-      console.log("should be showing scrolling down", scrolling_down)
-    }
   };
-
-  //var offset = 0.0;
-  // const setOffset = ({ direction }) => {
-  //   if (direction === 'up') { //&& data === this.state.steps.name
-  //     offset = 0.3
-  //   } else{
-  //     offset = 0.9
-  //   }
-  // }; 
 
   return (
     <div style={styles.outer}>
@@ -75,17 +56,10 @@ const PageStories = () => {
             <Step data={stepIndex} key={stepIndex}>
               <div style={styles.step}>
                 {/* TODO: This div contains the scrolling elements. What should you replace this with? */}
-<<<<<<< HEAD
                 {/* I'm a Scrollama Step of index {currentStepIndex}, */}
-                <h1 style= {{color:person.color}}> {person.name}</h1> {/* why didn't this work with stepIndex :(((()))) */}
+                <Styled.h1 style= {{color:person.color}}> {person.name}</Styled.h1> {/* why didn't this work with stepIndex :(((()))) */}
                 <p> Harvard {person.year}, {person.concentration},  {person.pronouns} </p>
                 <p> Quotes (just prints whole array rn): {person.quotes} </p>
-=======
-                {/*I'm a Scrollama Step of index {currentStepIndex}, */}
-                <Styled.h1 style= {{color:people[currentStepIndex].color}}> {people[currentStepIndex].name}</Styled.h1> {/* why didn't this work with stepIndex :(((()))) */}
-                <p> Harvard {people[currentStepIndex].year}, {people[currentStepIndex].concentration},  {people[currentStepIndex].pronouns} </p>
-                <p> Quotes (just prints whole array rn): {people[currentStepIndex].quotes}</p>
->>>>>>> 16524d4637a3222345b84c657a4ebfde1e5c5640
               </div>
             </Step>
           ))}
