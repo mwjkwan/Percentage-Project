6c16b5a34a518fd778c593380af28b559e202973
laurenc8 <laurenc8822@gmail.com>
2021-01-17 03:03:00 -0500

    added paddingTop

--- src/App.js
+++ src/App.js
@@ -2,37 +2,22 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import PageStories from './pages/PageStories';
 import PageData from './pages/PageData';
 import PageGallery from './pages/PageGallery';
-import { Navbar, Nav } from 'react-bootstrap'
-import "./Navbar.css";
+import NavigationBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar expand="lg">
-        <Navbar.Brand>The % Project</Navbar.Brand>
-        <Nav>
-          <Nav.Link href="/gallery">03. Gallery</Nav.Link>
-          <Nav.Link href="/data">02. Data</Nav.Link>
-          <Nav.Link href="/">01. Stories</Nav.Link>
-        </Nav>
-      </Navbar>
-      <Switch>
-        <Route exact path="/" component={PageStories} />
-        <Route exact path="/data" component={PageData} />
-        <Route exact path="/gallery" component={PageGallery} />
-        <Route component={() => <Redirect to="/" />} />
-      </Switch>
+      <NavigationBar />
+      <div style={{paddingTop: '65px'}}>
+        <Switch>
+          <Route exact path="/" component={PageStories} />
+          <Route exact path="/data" component={PageData} />
+          <Route exact path="/gallery" component={PageGallery} />
+          <Route component={() => <Redirect to="/" />} />
+        </Switch>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-/* <Navbar bg="dark" variant="pills" expand="lg" sticky="top" style={{position: "absolute", width: "100%"}}>
-                        <Nav className='navigation' class="navbar navbar-expand-md navbar-fixed-top">
-
-                            <Nav.Link onSelect={this.logout} href="/login">Logout</Nav.Link> 
-                            <Nav.Link id="ghome" href="/home"  >Home</Nav.Link>
-                            <Nav.Link id="bgroup" href="/groups" >Find a Group</Nav.Link>
-                            <Nav.Link href="/goal">Goal Page</Nav.Link>
-                            {/* <Button bsStyle="primary">Login</Button> */
